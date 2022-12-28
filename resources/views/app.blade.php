336348f0c8e429e7a11fc6f33aa38fbc83e159af
Hasnain <ahmad.hasnain@therightsw.com>
2022-12-28 22:37:53 +0500

    Theme setup

---
+++ resources/views/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Scripts -->
+        @routes
+        <script src="{{ mix('js/app.js') }}" defer></script>
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+
+        <!-- Core Vendors JS -->
+        <script src="{{asset('assets/js/vendors.min.js')}}"></script>
+        <!-- page js -->
+        <script src="assets/vendors/chartjs/Chart.min.js"></script>
+        <script src="assets/js/pages/dashboard-default.js"></script>
+        <!-- Core JS -->
+        <script src="{{asset('assets/js/app.min.js')}}"></script>
+
+        @env ('local')
+            <script src="http://localhost:8080/js/bundle.js"></script>
+        @endenv
+
+    </body>
+</html>
